e5cfe8821e2a214fcf188d6b19ad01389f816e74
Oleg Pronovich <oleg.pronovich@gmail.com>
2017-04-26 15:45:03 +0300

    add test StringMergerTest

--- test/com/pronovich/codewars/StringMergerTest.java
+++ test/com/pronovich/codewars/StringMergerTest.java
@@ -1,4 +1,4 @@
-package tes;
+package com.pronovich.codewars;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -7,33 +7,41 @@ public class StringMergerTest {
 
     @Test
     public void normalHappyFlow() {
-        assertTrue("codewars can be created from code and wars", StringMerger.isMerge("codewars", "code", "wars"));
-        assertTrue("codewars can be created from cdwr and oeas", StringMerger.isMerge("codewars", "cdwr", "oeas"));
-        assertFalse("Codewars are not codwars", StringMerger.isMerge("codewars", "cod", "wars"));
+        assertTrue("codewars can be created from code and wars",
+                StringMerger.isMerge("codewars", "code", "wars"));
+        assertTrue("codewars can be created from cdwr and oeas",
+                StringMerger.isMerge("codewars", "cdwr", "oeas"));
+        assertFalse("Codewars are not codwars",
+                StringMerger.isMerge("codewars", "cod", "wars"));
     }
 
     @Test
     public void canHandleCharactersInWrongOrder() {
-        assertFalse("codewars can't be created from code and wasr", StringMerger.isMerge("codewars", "code", "wasr"));
+        assertFalse("codewars can't be created from code and wasr",
+                StringMerger.isMerge("codewars", "code", "wasr"));
     }
 
     @Test
     public void canHandleExtraChars() {
-        assertFalse("codewars is not codewarss", StringMerger.isMerge("codewars", "code", "warss"));
+        assertFalse("codewars is not codewarss",
+                StringMerger.isMerge("codewars", "code", "warss"));
     }
 
     @Test
     public void evenMoreRandomCases() {
-        assertTrue("40\\EH= t<\\pcKqKl6q_q", StringMerger.isMerge("40\\EH= t<\\pcKqKl6q_q", "\\<K_q", "40EH= t\\pcKql6q"));
+        assertTrue("40\\EH= t<\\pcKqKl6q_q",
+                StringMerger.isMerge("40\\EH= t<\\pcKqKl6q_q", "\\<K_q", "40EH= t\\pcKql6q"));
     }
 
     @Test
     public void evenMoreRandomCases2() {
-        assertTrue("KqKl6q_q", StringMerger.isMerge("KqKl6q_q", "K_q", "Kql6q"));
+        assertTrue("KqKl6q_q",
+                StringMerger.isMerge("KqKl6q_q", "K_q", "Kql6q"));
     }
 
     @Test
     public void correctTest() {
-        assertTrue("abcdeabc is correct for ", StringMerger.isMerge("abcdefabc", "abcdef", "abc"));
+        assertTrue("abcdeabc is correct for ",
+                StringMerger.isMerge("abcdefabc", "abcdef", "abc"));
     }
 }
